2451e9a0abe3f4bff0ac00b364135fb5d8c990ba
Bastian Hoyer <dafire@gmail.com>
2023-02-02 17:26:56 +0100

    fix exports

--- fastapi_jinja/__init__.py
+++ fastapi_jinja/__init__.py
@@ -2,6 +2,6 @@
 
 __version__ = "0.1.0"
 __author__ = "Marc Brooks <marcwbrooks@gmail.com>"
-__all__ = []
+__all__ = ['template', 'render_template', 'global_init']
 
-from .engine import global_init, response, template
+from .engine import global_init, response, template, render_template
